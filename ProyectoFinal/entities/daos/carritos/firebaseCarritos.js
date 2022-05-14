474320735311d539922e75c6ea7d17bb82cd83fb
delgadoagustin <delgadoagustin147@gmail.com>
2022-05-13 21:28:42 -0300

    .gitignore fix

--- ProyectoFinal/entities/daos/carritos/firebaseCarritos.js
+++
@@ -1,7 +0,0 @@
-import ContenedorFirebase from '../../contenedores/ContenedorFirebase.js'
-
-export default class FirebaseCarritos extends ContenedorFirebase {
-  constructor() {
-    super('carritos')
-  }
-}
--- ProyectoFinal/entities/daos/productos/firebaseProductos.js
+++
@@ -1,7 +0,0 @@
-import ContenedorFirebase from '../../contenedores/ContenedorFirebase.js'
-
-export default class FirebaseProductos extends ContenedorFirebase {
-  constructor() {
-    super('productos')
-  }
-}
